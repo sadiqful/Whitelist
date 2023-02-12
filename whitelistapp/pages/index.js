b952b169beaa4dc7f3d8dda9a88628d022e1dd1c
Aliyu <sadiqful@gmail.com>
2023-02-12 16:50:54 +0100

    bug fixes

--- whitelistapp/pages/index.js
+++ whitelistapp/pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 import Web3Modal from 'web3modal';
 import {Contract, contract, ethers, providers} from 'ethers';
 import { useEffect, useRef, useState } from 'react';
-import { WHITELIST_CONTRACT_ADDRESS, abi } from '.../constants';
+import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants';
 
 export default function Home() {
   const [ walletConnected, setWalletConnected] = useState(false);
